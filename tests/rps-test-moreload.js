4358abd26c0466cfb670b38436d75f0ba3aadbd9
Ekaterina.Makarova <ma_ka_r0va@bk.ru>
2024-04-06 10:16:08 +0200

    Add .gitignore to root, make settings of rps-test-moreload.js similar to rps-test-index.js

--- tests/rps-test-moreload.js
+++ tests/rps-test-moreload.js
@@ -2,14 +2,16 @@ import {sleep} from 'k6';
 import http from 'k6/http';
 
 export const options = {
-    stages: [
-        {duration: '5s', target: 50},
-        {duration: '5s', target: 50},
-        {duration: '10s', target: 500},
-        {duration: '10s', target: 500},
-        {duration: '10s', target: 1500},
-        {duration: '10s', target: 1500}
-    ],
+    scenarios: {
+            low: {
+            executor: 'constant-arrival-rate',
+            rate: 350,
+            duration: '60s',
+            preAllocatedVUs: 100,
+            maxVUs: 3000,
+            timeUnit: '1s',
+        }
+    },
     thresholds: {
         http_req_failed: [{threshold: 'rate<0.01', abortOnFail: true, delayAbortEval: "3s"}],
         http_req_duration: [{threshold: 'med < 100', abortOnFail: true, delayAbortEval: "3s"}]
